8bdcbdf46353a74b9afe70d7d50812ff08f2ad62
yash suthar <yashsuthar883@gmail.com>
2026-04-06 15:43:30 +0530

    fix: corrected case sensitivity for Navbar import in Login.jsx

--- FRONTEND/src/components/admin/PostJob.jsx
+++ FRONTEND/src/components/admin/PostJob.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import Navbar from "../shared/Navbar";
+import Navbar from "../shared/navbar";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
